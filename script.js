917d7617a1fd68855307bb0b833f5af86a91109b
aoctopus08-cyber <aoctopus08@gmail.com>
2026-09-17 12:18:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const UNLOCK_DATE = new Date("2026-09-22T17:08:04");
+const UNLOCK_DATE = new Date("2026-09-22T20:05:00");
 
 const lockScreen = document.getElementById("lockScreen");
 const site = document.getElementById("site");
